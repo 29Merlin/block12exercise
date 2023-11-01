ca092f010ac7097b23ab4cdc7b24908c0b992874
29Merlin <wileke29@gmail.com>
2023-11-01 14:58:00 +0100

    Uppdat 1

--- src/exercises/ex2arrays/Arrays.java
+++ src/exercises/ex2arrays/Arrays.java
@@ -1,36 +1,55 @@
 package exercises.ex2arrays;
+import java.util.Scanner;
+import static java.lang.System.in;
 
 public class Arrays {
-
     /* The input functions */
+    public static final Scanner input = new Scanner(in);
     public static int[] getArrayOfIntsFromUser() {
-        // TODO
-        return new int[5];
+        System.out.println("Pls input 5 values");
+        String str1 = input.nextLine();
+        String[] str2 = str1.split(" ",0);
+        int[] outlist = new int[5];
+        for(int x = 0; 5 > x; x++) {
+            outlist[x] = Integer.parseInt(str2[x]);
+        }
+
+        return outlist;
     }
 
     public static int getValueFromUser() {
-        // TODO
-        return 0;
+        int Value = input.nextInt();
+        return Value;
     }
 
     /* The output functions */
     public static void printIndexToUser(int the_index, int the_value) {
-        // TODO
+        System.out.println("Value" + the_value + "is at index:" + the_index);
     }
 
     public static void printNotFoundToUser(int the_value) {
-        // TODO
+        System.out.println("Value" + the_value + "not found");
     }
 
     public static void printResultToUser(int the_index, int the_value) {
-        // TODO
+        if(the_index == null){
+            printNotFoundToUser(the_value);
+        }
+        else {
+            printIndexToUser(the_index, the_value);
+        }
     }
 
 
+
     /* The Algorithm */
-    public static int findIndexOfValue(int[] a_list, int a_value) {
-        // TODO
-        return 0;
+    public static Integer findIndexOfValue(int[] a_list, int a_value) {
+        for(int i = 0; i<a_list.length;i++) {
+            if (a_list[i] == a_value) {
+                return i;
+            }
+        }
+        return null;
     }
 
     /* The top-level behavior */
